1c6afa8c882907efbfc715733599fb1f39d2f070
SorerBOT <alonfiller@gmail.com>
2026-01-24 17:25:48 +0200

    FEAT printing nicer messages

--- bootloader_stage_2.asm
+++ bootloader_stage_2.asm
@@ -1,28 +1,25 @@
 [BITS 16]
 [ORG 0x7E00]
 
+%define CLRF 0x0D, 0x0A
+%define OS_STATUS "[sOS]"
+
 start:
+    xor ax, ax
+    mov ds, ax
+    mov si, msg_stage_2
+print_msg:
+    mov al, [si]
+    test al, al
+    jz print_msg_finished
     mov ah, 0x0E    ; BIOS Teletype Output
-    mov al, 'H'
     int 0x10
-    mov al, 'e'
-    int 0x10
-    mov al, 'l'
-    int 0x10
+    inc si
+    jmp print_msg
 
-    mov al, 'l'
-    int 0x10
-    mov al, 'o'
-    int 0x10
-    mov al, ' '
-    int 0x10
-    mov al, 'M'
-    int 0x10
-    mov al, 'a'
-    int 0x10
-    mov al, 'n'
-    int 0x10
-    mov al, '!'
-    int 0x10
 
+print_msg_finished:
     jmp $      ; Hang forever
+
+
+msg_stage_2 db OS_STATUS, ": booting...", CLRF, OS_STATUS, ": stage 1 completed...", CLRF, OS_STATUS, ": running stage 2...", 0
