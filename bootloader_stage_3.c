55640cc87755180e137f5f2603c0dc56fea697d4
SorerBOT <alonfiller@gmail.com>
2026-01-25 14:54:04 +0200

    FEAT wrote a simple VGA driver

--- bootloader_stage_3.c
+++ bootloader_stage_3.c
@@ -1,33 +1,14 @@
 __asm__(".code32\n");
 
-#define VGA_BUFFER 0xB8000
-#define WHITE_ON_BLACK 0x05
-#define PREVIOUS_STAGES_LINES_USED 12
-
-void print_char(char c, int col, int row)
-{
-    volatile char* video = (volatile char*)VGA_BUFFER;
-    int index = (row * 80 + col) * 2; // 2 bytes per char (char + color)
-    video[index] = c;
-    video[index + 1] = WHITE_ON_BLACK;
-}
-
-void print_string(const char* str, int row)
-{
-    int col = 0;
-    while (*str) {
-        print_char(*str, col, row);
-        col++;
-        str++;
-    }
-}
+#define VGA_DRIVER_IMPLEMENTATION
+#include "drivers/32-bit/vga_driver.h"
 
 void bootloader_stage_3()
 {
-    print_string("[sOS]: stage 2 completed...", PREVIOUS_STAGES_LINES_USED);
-    print_string("[sOS]: successfully entered protected 32-bit mode.", PREVIOUS_STAGES_LINES_USED + 1);
-    print_string("[sOS]: running stage 2...", PREVIOUS_STAGES_LINES_USED + 2);
-
+    VGA_DRIVER_report("[sOS]: stage 2 completed...", VGA_DRIVER_SUCCESS);
+    VGA_DRIVER_report("[sOS]: successfully entered protected 32-bit mode.", VGA_DRIVER_SUCCESS);
+    VGA_DRIVER_report("[sOS]: running stage 2...", VGA_DRIVER_SUCCESS);
+    VGA_DRIVER_report("[sOS]: running stage 2...", VGA_DRIVER_FAILURE);
 
     // Hang forever
     while(1);
---
+++ drivers/32-bit/vga_driver.h
@@ -0,0 +1,90 @@
+__asm__(".code32\n");
+
+#ifndef VGA_DRIVER_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+#define VGA_DRIVER_BUFFER_ADDRESS 0xB8000
+#define VGA_DRIVER_WHITE_ON_BLACK 0x05
+#define VGA_DRIVER_INITIAL_LINE 13
+#define VGA_DRIVER_WIDTH 80
+#define VGA_DRIVER_HEIGHT 25
+
+typedef enum
+{
+    VGA_DRIVER_SUCCESS,
+    VGA_DRIVER_FAILURE
+} VGA_Driver_report_status;
+
+void VGA_DRIVER_report(const char* message, VGA_Driver_report_status status);
+void VGA_DRIVER_printf(const char* format, ...);
+void VGA_DRIVER_clear();
+
+
+#endif /* VGA_DRIVER_H */
+
+#ifdef VGA_DRIVER_IMPLEMENTATION
+
+static size_t line = VGA_DRIVER_INITIAL_LINE;
+static size_t offset = 0;
+
+static void print_char(char character, uint8_t color);
+static void print_string(const char* string, uint8_t color);
+
+static void print_char(char character, uint8_t color)
+{
+    if (character == '\n')
+    {
+        ++line;
+        offset = 0;
+        return;
+    }
+    volatile uint8_t* buffer_address = (volatile uint8_t*) VGA_DRIVER_BUFFER_ADDRESS;
+    if (line >= VGA_DRIVER_HEIGHT && offset >= VGA_DRIVER_WIDTH)
+    {
+        // need to scroll
+        return;
+    }
+    else
+    {
+        size_t index = (line * VGA_DRIVER_WIDTH + offset) * 2;
+        buffer_address[index] = (uint8_t)character;
+        buffer_address[index + 1] = color;
+        ++offset;
+    }
+
+}
+
+static void print_string(const char* string, uint8_t color)
+{
+    while (*string != '\0')
+    {
+        print_char(*string, color);
+        ++string;
+    }
+}
+
+void VGA_DRIVER_report(const char* message, VGA_Driver_report_status status)
+{
+    print_char('[', VGA_DRIVER_WHITE_ON_BLACK);
+    if (status == VGA_DRIVER_SUCCESS)
+    {
+        print_string(" SUCCESS ", 0x02);
+    }
+    else if (status == VGA_DRIVER_FAILURE)
+    {
+        print_string(" FAILURE ", 0x04);
+    }
+    print_string("] ", VGA_DRIVER_WHITE_ON_BLACK);
+    print_string(message, VGA_DRIVER_WHITE_ON_BLACK);
+    print_char('\n', VGA_DRIVER_WHITE_ON_BLACK);
+
+}
+void VGA_DRIVER_printf(const char* format, ...)
+{
+
+}
+
+#endif /* VGA_DRIVER_IMPLEMENTATION */
