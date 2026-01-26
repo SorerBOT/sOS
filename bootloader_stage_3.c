b87e24bf290b7a919b4cc6a34f0e538caeb096a7
SorerBOT <alonfiller@gmail.com>
2026-01-26 15:54:10 +0200

    FEAT using my custom printf function

--- bootloader_stage_3.c
+++ bootloader_stage_3.c
@@ -9,6 +9,7 @@ void bootloader_stage_3()
     VGA_DRIVER_report("[sOS]: successfully entered protected 32-bit mode.", VGA_DRIVER_SUCCESS);
     VGA_DRIVER_report("[sOS]: running stage 2...", VGA_DRIVER_SUCCESS);
     VGA_DRIVER_report("[sOS]: running stage 2...", VGA_DRIVER_FAILURE);
+    VGA_DRIVER_printf("%s, %d, %c\n", "HELLO THERE", -123, 'z');
 
     // Hang forever
     while(1);
