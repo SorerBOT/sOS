20b6128cd987b2531255a96c8eb0e928caee8192
SorerBOT <alonfiller@gmail.com>
2026-02-10 11:42:31 +0200

    FEAT added identity-mapped page tables

--- bootloader_stage_3.c
+++ bootloader_stage_3.c
@@ -1,14 +1,141 @@
 __asm__(".code32\n");
 
+#include <stddef.h>
+
 #define VGA_DRIVER_IMPLEMENTATION
 #include "drivers/32-bit/vga_driver.h"
+
+#define KiB (1024)
+#define PAGE_SIZE (4 * KiB)
+#define FLAGS 0b11
+
+#define BASE_PAGE_TABLE_ADDRESS 0x1000000
+#define MEMORY_SIZE_TO_MAP 0x400000000
+#define BASE_PHYSICAL_ADDRESS 0x000000
+
+#define PAGES_IN_PT 512
+#define PAGES_IN_PDT (PAGES_IN_PT * 512)
+#define PAGES_IN_PDPT (PAGES_IN_PDT * 512)
+
+void* kmemset(void *address, int value_int, size_t len)
+{
+    unsigned char value = (unsigned char) value_int;
+    unsigned char* data = address;
+    for (size_t i = 0; i < len; ++i)
+    {
+        data[i] = value;
+    }
+
+    return address;
+}
+
+typedef uint64_t lm_pointer;
+
+typedef struct
+{
+    char data[PAGE_SIZE];
+} Frame_t;
+
+typedef struct
+{
+    Frame_t* mapped_frame_physical_address;
+} Page_t;
+
+typedef struct
+{
+    lm_pointer pages[PAGE_SIZE / sizeof(lm_pointer)];
+} PT_t;
+
+typedef struct
+{
+    lm_pointer pts[PAGE_SIZE / sizeof(lm_pointer)];
+} PDT_t;
+
+typedef struct
+{
+    lm_pointer pdts[PAGE_SIZE / sizeof(lm_pointer)];
+} PDPT_t;
+
+typedef struct
+{
+    lm_pointer pdpts[PAGE_SIZE / sizeof(lm_pointer)];
+} PML4T_t;
+
+lm_pointer next_physical_address = BASE_PHYSICAL_ADDRESS;
+lm_pointer next_free_address = BASE_PAGE_TABLE_ADDRESS;
+
+lm_pointer get_next_free_address()
+{
+    kmemset((void*)next_free_address, 0, PAGE_SIZE);
+    lm_pointer temp = next_free_address;
+    next_free_address += PAGE_SIZE;
+    return temp;
+}
+
+lm_pointer get_next_physical_address()
+{
+    lm_pointer temp = next_physical_address;
+    next_physical_address += PAGE_SIZE;
+    return temp;
+}
+
+void PT_init_identity_map(PT_t* pt, size_t pages_count)
+{
+    size_t created_pages_count = 0;
+    for (; pages_count > 0 && created_pages_count < 512; --pages_count, ++created_pages_count)
+    {
+        pt->pages[created_pages_count] = (get_next_physical_address() | FLAGS);
+    }
+}
+
+void PDT_init_identity_map(PDT_t* pdt, size_t pages_count)
+{
+    size_t created_pt_count = 0;
+    for (; pages_count > 0 && created_pt_count < 512; pages_count -= PAGES_IN_PT, ++created_pt_count)
+    {
+        pdt->pts[created_pt_count] = get_next_free_address();
+        PT_init_identity_map((PT_t*)pdt->pts[created_pt_count], pages_count);
+        pdt->pts[created_pt_count] |= FLAGS;
+    }
+}
+
+void PDPT_init_identity_map(PDPT_t* pdpt, size_t pages_count)
+{
+    size_t created_pdt_count = 0;
+    for (; pages_count > 0 && created_pdt_count < 512; pages_count -= PAGES_IN_PDT, ++created_pdt_count)
+    {
+        pdpt->pdts[created_pdt_count] = get_next_free_address();
+        PDT_init_identity_map((PDT_t*)pdpt->pdts[created_pdt_count], pages_count);
+        pdpt->pdts[created_pdt_count] |= FLAGS;
+    }
+}
+
+PML4T_t* PML4T_init_identity_map(size_t memory_size_to_map)
+{
+    PML4T_t* pml4t = (PML4T_t*) get_next_free_address();
+
+    size_t pages_count = memory_size_to_map / PAGE_SIZE;
+
+    size_t created_pdpt_count = 0;
+    for (; pages_count > 0 && created_pdpt_count < 512; pages_count -= PAGES_IN_PDPT, ++created_pdpt_count)
+    {
+        pml4t->pdpts[created_pdpt_count] = get_next_free_address();
+        PDPT_init_identity_map((PDPT_t*)pml4t->pdpts[created_pdpt_count], pages_count);
+        pml4t->pdpts[created_pdpt_count] |= FLAGS;
+    }
+
+    return pml4t;
+}
 
 void bootloader_stage_3()
 {
     VGA_DRIVER_report("[sOS]: stage 2 completed...", VGA_DRIVER_SUCCESS);
     VGA_DRIVER_report("[sOS]: successfully entered protected 32-bit mode.", VGA_DRIVER_SUCCESS);
-    VGA_DRIVER_report("[sOS]: running stage 2...", VGA_DRIVER_SUCCESS);
-    VGA_DRIVER_report("[sOS]: running stage 2...", VGA_DRIVER_FAILURE);
+
+
+    PML4T_t* pml4t = PML4T_init_identity_map(MEMORY_SIZE_TO_MAP);
+
+
 
     // Hang forever
     while(1);
