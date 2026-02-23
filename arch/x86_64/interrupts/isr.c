1fd5f80936212dcc9a5cefd0dd26ccbfd6c8afcc
SorerBOT <alonfiller@gmail.com>
2026-02-23 19:51:00 +0200

    FEAT added more handlers

--- arch/x86_64/interrupts/isr.c
+++ arch/x86_64/interrupts/isr.c
@@ -3,15 +3,18 @@
 
 enum
 {
+    ISR_DIVIDE_BY_ZERO = 0x00,
     ISR_BREAKPOINT = 0x03,
-    ISR_PAGE_FAULT = 0x0E
+    ISR_DOUBLE_FAULT = 0x08,
+    ISR_PAGE_FAULT = 0x0E,
+    ISR_GENERAL_PROTECTION_FAULT = 0x0D
 };
 
 static void isr_dump_registers(const isr_args_t* args);
 
 static void isr_dump_registers(const isr_args_t* args)
 {
-    console_io_print_blue_screen(
+    console_io_printf(
             "Registers dump:\n"
             "%3s: %08x\n"
             "%3s: %08x        %3s: %08x\n"
@@ -39,11 +42,20 @@ void isr_handler(isr_args_t* args)
 {
     switch ( args->isr_number )
     {
+        case ISR_DIVIDE_BY_ZERO:
+            console_io_print_blue_screen("DIVIDE BY ZERO OCCURRED:\n");
+            isr_dump_registers(args);
+            break;
         case ISR_BREAKPOINT:
             console_io_print_blue_screen("Breakpoint on instruction %p reached.\n", args->rip);
+            break;
+        case ISR_DOUBLE_FAULT:
+            console_io_print_blue_screen("DOUBLE FAULT OCCURRED:\n");
             isr_dump_registers(args);
             break;
         case ISR_PAGE_FAULT:
+            console_io_print_blue_screen("PAGE FAULT OCCURRED:\n");
+            isr_dump_registers(args);
             break;
     }
 }
