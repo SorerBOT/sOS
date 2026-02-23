fb685fa97e446317a9faea480b8ef9c25910166d
SorerBOT <alonfiller@gmail.com>
2026-02-23 12:10:13 +0200

    FEAT updated build process to use a makefile

---
+++ Makefile
@@ -0,0 +1,49 @@
+# Makefile for compiling the kernel
+ARCH=x86_64
+DRIVERS_DIR=drivers
+ARCH_DIR=arch
+LIBC_DIR=libc
+BIN_DIR=bin
+
+INCLUDE_FLAGS = -Ilibc/include -Idrivers/$(ARCH)/include -Iarch/$(ARCH)/include -Iarch/common/include
+CFLAGS = -std=c99 -ffreestanding -mno-red-zone -m64 -g $(INCLUDE_FLAGS)
+
+BIN_64_BIT_DIR=$(BIN_DIR)/64-bit
+
+KERNEL_ORG = 0x10000 # 0x7E00 + 32.5KiB
+
+KERNEL_DIR = kernel
+KERNEL_START = $(KERNEL_DIR)/kernel_start
+KERNEL = $(KERNEL_DIR)/kernel
+
+KERNEL_SRCS := $(shell find $(KERNEL_DIR) -type f \( -name '*.c' -o -name '*.asm' \))
+ARCH_SRCS := $(shell find $(ARCH_DIR)/$(ARCH) -type f \( -name '*.c' -o -name '*.asm' \))
+DRIVERS_SRCS := $(shell find $(DRIVERS_DIR)/$(ARCH) -type f \( -name '*.c' -o -name '*.asm' \))
+LIBC_SRCS := $(shell find $(LIBC_DIR) -type f \( -name '*.c' -o -name '*.asm' \))
+
+ALL_SRCS = $(KERNEL_SRCS) $(KERNEL_START_SRC) $(ARCH_SRCS) $(DRIVERS_SRCS) $(LIBC_SRCS)
+
+C_OBJS := $(patsubst %.c, $(BIN_64_BIT_DIR)/%.o, $(filter %.c, $(ALL_SRCS)))
+ASM_OBJS := $(patsubst %.asm, $(BIN_64_BIT_DIR)/%.o, $(filter %.asm, $(ALL_SRCS)))
+ALL_OBJS := $(ASM_OBJS) $(C_OBJS)
+START_OBJ := $(BIN_64_BIT_DIR)/$(KERNEL_START).o
+ALL_OBJS_EXCEPT_START := $(filter-out $(START_OBJ), $(ALL_OBJS))
+
+$(BIN_64_BIT_DIR)/$(KERNEL).bin: $(ALL_OBJS)
+	@echo "Linking the kernel..."
+	x86_64-elf-ld -m elf_x86_64 --oformat binary -Ttext $(KERNEL_ORG) \
+		-o $(BIN_64_BIT_DIR)/$(KERNEL).bin \
+		$(START_OBJ) $(ALL_OBJS_EXCEPT_START)
+
+$(BIN_64_BIT_DIR)/%.o: %.c
+	@echo "Compiling C file $<"
+	@mkdir -p $(dir $@)
+	x86_64-elf-gcc $(CFLAGS) -c $< -o $@
+
+$(BIN_64_BIT_DIR)/%.o: %.asm
+	@echo "Compiling ASM file $<"
+	@mkdir -p $(dir $@)
+	nasm -f elf64 $< -o $@
+
+clean:
+	rm -rf $(BIN_64_BIT_DIR)
--- runOS.sh
+++ runOS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OS_IMG="os-img"
-OS_IMAGE_SIZE_MAX=66560 # 33KiB + 32 KiB
+OS_IMAGE_SIZE_MAX=66560 # 33KiB + 32KiB
 
 ARCH="x86_64"
 DRIVERS_DIR="drivers"
@@ -91,7 +91,7 @@ cat "$BIN_16_BIT_DIR/$STAGE_1.bin" "$BIN_32_BIT_DIR/$STAGE_2.bin" > "$BIN_DIR/$O
 BOOTLOADER_SIZE=$(wc -c < "$BIN_DIR/$OS_IMG.bin")
 echo "Bootloader built successfully..."
 if (( BOOTLOADER_SIZE > BOOTLOADER_SIZE_MAX )); then
-    echo "Bootloader size: $BOOTLOADER_SIZE, exceeds allowed size of 16KiB + 512 bytes. It's time to make the BIOS load more code in stage 1..."
+    echo "Bootloader size: $BOOTLOADER_SIZE, exceeds allowed size of 33KiB bytes. It's time to make the BIOS load more code in stage 1..."
     exit 1
 else
     echo "Bootloader size: $BOOTLOADER_SIZE is OK."
@@ -100,36 +100,9 @@ fi
 echo "Padding bootloader binary (entire OS image)..."
 truncate -s $BOOTLOADER_SIZE_MAX "$BIN_DIR/$OS_IMG.bin"
 
+# Compiling and linking the kernel via a makefile
+make "$BIN_64_BIT_DIR/$KERNEL.bin"
 
-echo "Compiling the kernel..."
-CFLAGS_64_BIT="-std=c99 -ffreestanding -mno-red-zone -m64 -g $INCLUDE_FLAGS"
-nasm -f elf64 "$KERNEL_START.asm" -o "$BIN_64_BIT_DIR/$KERNEL_START.o"
-x86_64-elf-gcc $CFLAGS_64_BIT -c "$KERNEL.c" -o "$BIN_64_BIT_DIR/$KERNEL.o"
-
-echo "Compiling 64-bit drivers..."
-for file in "$DRIVERS_DIR/$ARCH"/*.c; do
-    [ -e "$file" ] || continue
-    x86_64-elf-gcc $CFLAGS_64_BIT -c "$file" -o "$BIN_64_BIT_DIR/$(dirname "$file")/$(basename "$file" .c).o"
-done
-echo "Compiling 64-bit architecture specific code..."
-for file in "$ARCH_DIR/$ARCH"/*.c; do
-    [ -e "$file" ] || continue
-    x86_64-elf-gcc $CFLAGS_64_BIT -c "$file" -o "$BIN_64_BIT_DIR/$(dirname "$file")/$(basename "$file" .c).o"
-done
-echo "Compiling 64-bit libc..."
-for file in "$LIBC_DIR"/*.c; do
-    [ -e "$file" ] || continue
-    x86_64-elf-gcc $CFLAGS_64_BIT -c "$file" -o "$BIN_64_BIT_DIR/$(dirname "$file")/$(basename "$file" .c).o"
-done
-
-echo "Linking kernel..."
-OBJ_FILES_EXCEPT_ENTRY=$(find $BIN_64_BIT_DIR -type f -name "*.o" ! -name "$(basename "$BIN_64_BIT_DIR/$KERNEL_START.o")")
-echo "Printing object files:"
-echo $OBJ_FILES_EXCEPT_ENTRY
-x86_64-elf-ld -m elf_x86_64 --oformat binary -Ttext $KERNEL_ORG \
-    -o "$BIN_64_BIT_DIR/$KERNEL.bin" \
-    "$BIN_64_BIT_DIR/$KERNEL_START.o" $OBJ_FILES_EXCEPT_ENTRY
-echo "Successfully linked kernel..."
 
 echo "Appending kernel to OS image..."
 cat "$BIN_64_BIT_DIR/$KERNEL.bin" >> "$BIN_DIR/$OS_IMG.bin"
@@ -137,7 +110,7 @@ cat "$BIN_64_BIT_DIR/$KERNEL.bin" >> "$BIN_DIR/$OS_IMG.bin"
 OS_IMAGE_SIZE=$(wc -c < "$BIN_DIR/$OS_IMG.bin")
 echo "OS built successfully..."
 if (( OS_IMAGE_SIZE > OS_IMAGE_SIZE_MAX )); then
-    echo "OS size: $OS_IMAGE_SIZE, exceeds allowed size of 32KiB + 512 bytes. It's time to make the BIOS load more code in stage 1...."
+    echo "OS size: $OS_IMAGE_SIZE, exceeds allowed size of 65KiB. It's time to make the BIOS load more code in stage 1...."
     exit 1
 else
     echo "OS size: $OS_IMAGE_SIZE is OK. Running the OS"
