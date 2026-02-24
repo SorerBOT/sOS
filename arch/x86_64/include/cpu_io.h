14f407e880af6ff247da39ea9a530b8f7e87cde8
SorerBOT <alonfiller@gmail.com>
2026-02-24 11:56:31 +0200

    FEAT added inb

--- arch/x86_64/include/cpu_io.h
+++ arch/x86_64/include/cpu_io.h
@@ -3,9 +3,25 @@
 
 #include <types.h>
 
+static inline void cpu_io_outb(word port, byte value);
+static inline byte cpu_io_inb(word port);
+
+
 static inline void cpu_io_outb(word port, byte value)
 {
     __asm__ volatile ( "outb %0, %1" : : "a"(value), "Nd"(port) );
 }
+
+static inline byte cpu_io_inb(word port)
+{
+    byte value;
+    __asm__ volatile ( "inb %w1, %b0"
+            : "=a"(value)
+            : "Nd"(port)
+            : "memory");
+
+    return value;
+}
+
 
 #endif /* CPU_IO_H */
