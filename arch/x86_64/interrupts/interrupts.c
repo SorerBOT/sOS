c5aee8503a5584b90493135e60b42f6658e32c1a
SorerBOT <alonfiller@gmail.com>
2026-02-24 12:20:41 +0200

    FEAT finally enabled external interrupts after remapping the PIC

--- arch/x86_64/interrupts/interrupts.c
+++ arch/x86_64/interrupts/interrupts.c
@@ -1,11 +1,13 @@
 #include <interrupts.h>
 #include <types.h>
 #include "include/idt.h"
+#include "include/pic.h"
 
 extern void interrupts_enable(void);
 
 void interrupts_setup(void)
 {
     idt_setup();
-    //interrupts_enable();
+    pic_remap();
+    interrupts_enable();
 }
