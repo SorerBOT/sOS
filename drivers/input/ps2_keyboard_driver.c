3c073f1b42090ffca21d1f9308597701097b2dd3
SorerBOT <alonfiller@gmail.com>
2026-03-04 18:12:06 +0200

    FEAT added extended scancode 1 mapping

--- drivers/input/ps2_keyboard_driver.c
+++ drivers/input/ps2_keyboard_driver.c
@@ -23,11 +23,11 @@ static inline errors_t handle_state_normal(byte scancode, keyboard_event_t* even
 {
     if ( scancode < SCANCODE_1_EXTENDED_OFFSET )
     {
-        event->type = (scancode < SCANCODE_1_SINGLE_BYTE_MAKE_SCANCODES_COUNT)
+        event->type = (scancode < SCANCODE_1_NORMAL_MAKE_SCANCODES_COUNT)
             ? KEYBOARD_PRESSED
             : KEYBOARD_RELEASED;
 
-        byte make_scancode = scancode & ~SCANCODE_1_SINGLE_BYTE_MAKE_SCANCODES_COUNT;
+        byte make_scancode = scancode & ~SCANCODE_1_NORMAL_MAKE_SCANCODES_COUNT;
         event->keycode = map_normal_scancode_to_keycode[make_scancode];
 
         return ERRORS_NONE;
