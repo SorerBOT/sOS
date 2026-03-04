0d8d7dfaca12ac1a887fb62c339739fa91eeee29
SorerBOT <alonfiller@gmail.com>
2026-03-04 15:54:08 +0200

    REF: removed debug print statements from keyboard.c

--- arch/x86_64/keyboard.c
+++ arch/x86_64/keyboard.c
@@ -1,30 +1,12 @@
 #include <keyboard.h>
 #include <keyboard_driver.h>
 
-
-
-#include <console_io.h>
-
-
 void keyboard_read_char(char* c)
 {
     keyboard_unit_t unit;
     for (;;)
     {
         keyboard_driver_consume_unit(&unit);
-
-        //console_io_printf("Consumed keyboard unit.\n");
-
-        //if ( unit.unit_type == KEYBOARD_UNIT_ACTION )
-        //{
-        //    console_io_printf("Modifiers: %08b\n", unit.data.action.modifiers_state);
-        //    console_io_printf("Key: %02x\n", unit.data.action.key);
-        //    continue;
-        //}
-        //else
-        //{
-        //    console_io_printf("Character: %c\n", unit.data.character);
-        //}
 
         if ( unit.unit_type == KEYBOARD_UNIT_UNICODE )
         {
