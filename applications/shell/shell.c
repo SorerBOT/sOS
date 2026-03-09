71e2926ba9334e3b0ba27fd11e8e9d943e7de53b
SorerBOT <alonfiller@gmail.com>
2026-03-09 12:40:24 +0200

    FEAT added my neofetch (ascii art welcome for shell)

--- applications/shell/shell.c
+++ applications/shell/shell.c
@@ -2,6 +2,11 @@
 #include <shell.h>
 
 #define SHELL_BUFFER_SIZE 256
+#define SHELL_WELCOME_MSG (                                                 \
+"  _      __    __                     __             ____  ____\n"           \
+" | | /| / /__ / /______  __ _  ___   / /____    ___ / __ \\/ __/\n"          \
+" | |/ |/ / -_) / __/ _ \\/  ' \\/ -_) / __/ _ \\  (_-</ /_/ /\\ \\  \n"      \
+" |__/|__/\\__/_/\\__/\\___/_/_/_/\\__/  \\__/\\___/ /___/\\____/___/  \n" )
 
 char shell_buffer[SHELL_BUFFER_SIZE];
 
@@ -10,7 +15,7 @@ static inline void shell_execute_command(const char* command);
 
 static inline void shell_print_welcome(void)
 {
-    tty_print_line("Welcome to sOS shell!");
+    tty_print_line(SHELL_WELCOME_MSG);
 }
 
 static inline void shell_command_execute(const char* command)
