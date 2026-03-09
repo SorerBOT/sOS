b14f6a43adcf126a143ad81f73bed084b5562329
SorerBOT <alonfiller@gmail.com>
2026-03-09 20:46:35 +0200

    FEAT added an infinite_loop app.

---
+++ applications/include/infinite_loop.h
@@ -0,0 +1,6 @@
+#ifndef INFINITE_LOOP_H
+#define INFINITE_LOOP_H
+
+void infinite_loop_launch(int argc, char** argv);
+
+#endif /* INFINITE_LOOP_H */
---
+++ applications/infinite_loop/infinite_loop.c
@@ -0,0 +1,9 @@
+#include <infinite_loop.h>
+
+void infinite_loop_launch(int argc, char** argv)
+{
+    for (;;)
+    {
+
+    }
+}
