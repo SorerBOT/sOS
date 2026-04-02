e412e6e21d8092bbb4316491340e798971634a4f
SorerBOT <alonfiller@gmail.com>
2026-04-02 22:13:35 +0300

    FEAT added translation macros between physical addresses and virtual addresses

--- kernel/vmm/include/vmm.h
+++ kernel/vmm/include/vmm.h
@@ -4,8 +4,12 @@
 #include <types.h>
 #include <pmm.h>
 
-void* vmm_create_page_table(void);
+#define VMM_KERNEL_MAP_VIRTUAL_OFFSET 0xFFFF800000000000
+#define VMM_TRANSLATE_PHYSICAL_TO_KERNEL_MAP(physical_address) (((qword)(physical_address)) + VMM_KERNEL_MAP_VIRTUAL_OFFSET)
+#define VMM_TRANSLATE_KERNEL_MAP_TO_PHYSICAL(kernel_map_address) (((qword)(kernel_map_address)) - VMM_KERNEL_MAP_VIRTUAL_OFFSET)
+
 void vmm_setup(void);
+void* vmm_create_page_table(void);
 void vmm_page_allocate(void* _pml4t);
 void vmm_page_free(void* _pml4t);
 
