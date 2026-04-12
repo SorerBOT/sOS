1d990549e135e7f98836fa2197ff3747546ec355
SorerBOT <alonfiller@gmail.com>
2026-04-12 16:47:42 +0300

    PATCH: fixed error in kernel_allocator_setup

--- kernel/kernel_allocator/kernel_allocator.c
+++ kernel/kernel_allocator/kernel_allocator.c
@@ -1,12 +1,23 @@
 #include <kernel_allocator.h>
 #include <slab_allocator.h>
+#include <math_extended.h>
+
+#define KERNEL_ALLOCATR_MIN_ALLOCATION_SIZE_LOG_2 3
 
 static void* slab_allocators[16];
 
 void kernel_allocator_setup(void)
 {
-    for ( size_t i = 3; i < 16; ++i )
+    for ( size_t i = KERNEL_ALLOCATR_MIN_ALLOCATION_SIZE_LOG_2; i < 16; ++i )
     {
-        slab_allocators[i] = slab_allocator_init(i << 16);
+        slab_allocators[i] = slab_allocator_init(2 << i);
     }
+}
+
+void* kernel_allocator_allocate(size_t size)
+{
+    size_t log2 = math_extended_round_up_to_log_two(size);
+    log2 += (log2 < KERNEL_ALLOCATR_MIN_ALLOCATION_SIZE_LOG_2) * (KERNEL_ALLOCATR_MIN_ALLOCATION_SIZE_LOG_2 - log2);
+
+
 }
