d035f43e6ef64835d34224d3c63aa9331955da91
SorerBOT <alonfiller@gmail.com>
2026-04-12 16:44:27 +0300

    FEAT added popcount & math_extended_round_up_to_log_two

--- libc/sOS/include/math_extended.h
+++ libc/sOS/include/math_extended.h
@@ -4,6 +4,10 @@
 #include <types.h>
 #include <math.h>
 
+
 qword math_extended_round_up_to_power_of_two(qword x);
+qword math_extended_round_up_to_log_two(qword x);
+qword math_extended_right_smear(qword x);
+qword math_extended_popcount(qword x);
 
 #endif /* MATH_EXTENDED_H */
