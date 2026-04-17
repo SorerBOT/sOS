2f2a54268fa64b9df4f5c3baf7962e9b35d0db02
SorerBOT <alonfiller@gmail.com>
2026-04-17 09:25:07 +0300

    FEAT added cpu_io_read/write_word/dword

--- arch/common/include/cpu_io.h
+++ arch/common/include/cpu_io.h
@@ -3,8 +3,14 @@
 
 #include <types.h>
 
-void cpu_io_write_byte(word port, byte value);
 byte cpu_io_read_byte(word port);
+word cpu_io_read_word(word port);
+dword cpu_io_read_dword(word port);
+
+void cpu_io_write_byte(word port, byte value);
+void cpu_io_write_word(word port, word value);
+void cpu_io_write_dword(word port, dword value);
+
 void cpu_io_wait(void);
 
 #endif /* CPU_IO_H */
